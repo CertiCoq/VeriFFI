aa563d54460e7a9e1e8469b4aa1b2f4d37a5f809
Joomy Korkut <joomy@cs.princeton.edu>
2022-04-27 07:07:53 -0400

    Simple proofs between prim and model types

---
+++ library/modelled.v
@@ -0,0 +1,248 @@
+Require Import MetaCoq.Template.All.
+Require Import String.
+
+Require Import ZArith.
+Require Import Psatz.
+Require Import List.
+Import ListNotations.
+
+Class Modelled (prim_type model_type : Type) :=
+  { model_to_prim : model_type -> prim_type
+  ; prim_to_model : prim_type -> model_type
+  ; prim_to_model_to_prim : forall (x : prim_type), model_to_prim (prim_to_model x) = x
+  ; model_to_prim_to_model : forall (x : model_type), prim_to_model (model_to_prim x) = x
+  }.
+
+Require Import VeriFFI.library.meta.
+
+(* opaque and transparent *)
+Inductive annotated : Type :=
+| TYPEPARAM : (forall (A : Type) `{Rep A}, annotated) -> annotated
+| OPAQUE : forall (prim_type model_type : Type)
+                    `{Modelled prim_type model_type},
+                    (option (prim_type -> annotated)) ->
+                    annotated
+| TRANSPARENT : forall (A : Type) `{Rep A}, (option (A -> annotated)) -> annotated.
+
+Definition Rep_Z : Rep Z. Admitted.
+
+Theorem Rep_any : forall {A : Type}, Rep A. Admitted.
+
+Fixpoint to_prim_fn_type (r : annotated) : Type :=
+  match r with
+  | TYPEPARAM f => forall (A : Type), to_prim_fn_type (f A Rep_any)
+  | OPAQUE pt mt M o =>
+      match o with
+      | None => pt
+      | Some fp => forall (p : pt), to_prim_fn_type (fp p)
+      end
+  | TRANSPARENT t R o =>
+      match o with
+      | None => t
+      | Some f => forall (x : t), to_prim_fn_type (f x)
+      end
+  end.
+
+Fixpoint to_model_fn_type (r : annotated) : Type :=
+  match r with
+  | TYPEPARAM f => forall (A : Type), to_model_fn_type (f A Rep_any)
+  | OPAQUE pt mt M o =>
+      match o with
+      | None => mt
+      | Some fp => forall (m : mt), to_model_fn_type (fp (model_to_prim m))
+      end
+  | TRANSPARENT t R o =>
+      match o with
+      | None => t
+      | Some f => forall (x : t), to_model_fn_type (f x)
+      end
+  end.
+
+Record extern_properties :=
+  { type_desc : annotated
+  ; prim_fn : to_prim_fn_type type_desc
+  ; model_fn : to_model_fn_type type_desc
+  ; c_name : string
+  }.
+
+Fixpoint model_spec_aux
+         (a : annotated)
+         (ft : to_prim_fn_type a)
+         (mt : to_model_fn_type a) {struct a} : Prop.
+destruct a as [f|prim_type model_type M|A R o].
+* simpl in ft, mt.
+  exact (forall (A : Type), model_spec_aux (f A Rep_any) (ft A) (mt A)).
+* simpl in ft, mt.
+  destruct o as [pf|].
+  - refine (forall (x : prim_type), model_spec_aux (pf x) (ft x) _).
+    pose (m := mt (prim_to_model x)).
+    rewrite prim_to_model_to_prim in m.
+    exact m.
+  - exact (ft = model_to_prim mt).
+* simpl in ft, mt.
+  destruct o as [pf|].
+  - refine (forall (x : A), model_spec_aux (pf x) (ft x) (mt x)).
+  - exact (ft = mt).
+Defined.
+
+Definition model_spec (ep : extern_properties) : Prop :=
+  model_spec_aux (type_desc ep) (prim_fn ep) (model_fn ep).
+
+Module Type Int63.
+  Axiom t : Type.
+  Axiom from_Z : Z -> t.
+  Axiom to_Z : t -> Z.
+  Axiom add : t -> t -> t.
+  Axiom mul : t -> t -> t.
+End Int63.
+
+(* Axiom VSU : Type. *)
+(* Axiom int63_vsu : VSU. *)
+(* Axiom lift : forall {A : Type}, A -> VSU -> string -> A. *)
+
+Module FM <: Int63.
+  Definition t := {z : Z | 0 <= z < 2^63}%Z.
+  Definition from_Z (z : Z) : t.
+    exists (Z.rem (Z.abs z) (2^63)).
+    apply Z.rem_bound_pos_pos.
+    apply Z.pow_pos_nonneg.
+    constructor.
+    apply Pos2Z.is_nonneg.
+    apply Z.abs_nonneg.
+  Defined.
+
+  Definition to_Z (z : t) : Z := proj1_sig z.
+  Definition add (x y : t) : t.
+    destruct x as [xz x_pf], y as [yz y_pf].
+    exists ((xz + yz) mod (2^63))%Z.
+    split.
+    * destruct x_pf as [x_pf1 _], y_pf as [y_pf1 _].
+      apply Z_mod_nonneg_nonneg.
+      apply Z.add_nonneg_nonneg.
+      assumption. assumption.
+      apply Pos2Z.is_nonneg.
+    * apply Z.mod_pos_bound.
+      constructor.
+  Defined.
+
+  Definition mul (x y : t) : t.
+    destruct x as [xz x_pf], y as [yz y_pf].
+    exists ((xz * yz) mod (2^63))%Z.
+    split.
+    * destruct x_pf as [x_pf1 _], y_pf as [y_pf1 _].
+      apply Z_mod_nonneg_nonneg.
+      apply Z.mul_nonneg_nonneg.
+      assumption. assumption.
+      apply Pos2Z.is_nonneg.
+    * apply Z.mod_pos_bound.
+      constructor.
+  Defined.
+End FM.
+
+
+Module C : Int63.
+  Axiom t : Type.
+  Axiom from_Z : Z -> t.
+  Axiom to_Z : t -> Z.
+  Axiom add : t -> t -> t.
+  Axiom mul : t -> t -> t.
+End C.
+
+Module Int63_Proofs.
+  Definition Rep_Z : Rep Z. Admitted.
+  Axiom Modelled_t : Modelled C.t FM.t.
+
+  Definition from_Z_ep : extern_properties :=
+    {| type_desc :=
+        @TRANSPARENT Z Rep_Z
+           (Some (fun z => @OPAQUE _ _ Modelled_t None))
+     ; prim_fn := C.from_Z
+     ; model_fn := FM.from_Z
+     ; c_name := "int63_from_Z"
+     |}.
+
+  Definition to_Z_ep : extern_properties :=
+    {| type_desc :=
+        @OPAQUE _ _ Modelled_t
+           (Some (fun z => @TRANSPARENT Z Rep_Z None))
+     ; prim_fn := C.to_Z
+     ; model_fn := FM.to_Z
+     ; c_name := "int63_to_Z"
+     |}.
+
+  Definition add_ep : extern_properties :=
+    {| type_desc :=
+          @OPAQUE _ _ Modelled_t
+            (Some (fun i =>
+              @OPAQUE _ _ Modelled_t
+                (Some (fun i => @OPAQUE _ _ Modelled_t None))))
+     ; prim_fn := C.add
+     ; model_fn := FM.add
+     ; c_name := "int63_add"
+     |}.
+
+  Definition mul_ep : extern_properties :=
+    {| type_desc :=
+          @OPAQUE _ _ Modelled_t
+            (Some (fun i =>
+              @OPAQUE _ _ Modelled_t
+                (Some (fun i => @OPAQUE _ _ Modelled_t None))))
+     ; prim_fn := C.mul
+     ; model_fn := FM.mul
+     ; c_name := "int63_mul"
+     |}.
+
+  Axiom from_Z_properties : model_spec from_Z_ep.
+  Axiom to_Z_properties : model_spec to_Z_ep.
+  Axiom add_properties : model_spec add_ep.
+  Axiom mul_properties : model_spec mul_ep.
+
+  Ltac eq_refl_match :=
+    match goal with
+    | [ |- context[match ?x with | eq_refl => _ end] ] => destruct x
+    end.
+
+  Lemma seven : C.to_Z (C.add (C.from_Z 3%Z) (C.from_Z 4%Z)) = 7%Z.
+  Proof.
+    pose proof (FP := from_Z_properties).
+    pose proof (TP := to_Z_properties).
+    pose proof (AP := add_properties).
+    hnf in FP, TP, AP.
+    simpl in FP, TP, AP.
+    rewrite !FP, !TP, !AP.
+    unfold id, eq_rect in *.
+    repeat eq_refl_match.
+    repeat rewrite prim_to_model_to_prim, model_to_prim_to_model.
+    unfold FM.to_Z, FM.add, FM.from_Z.
+    simpl.
+    rewrite Z.mod_small.
+    auto.
+    lia.
+  Qed.
+
+
+  Lemma dist_add : forall (x y z : Z),
+    C.to_Z (C.mul (C.from_Z x) (C.add (C.from_Z y) (C.from_Z z))) =
+    C.to_Z (C.add (C.mul (C.from_Z x) (C.from_Z y)) (C.mul (C.from_Z x) (C.from_Z z))).
+  Proof.
+    intros x y z.
+    pose proof (FP := from_Z_properties).
+    pose proof (TP := to_Z_properties).
+    pose proof (AP := add_properties).
+    pose proof (MP := mul_properties).
+    hnf in FP, TP, AP, MP.
+    simpl in FP, TP, AP, MP.
+    rewrite ?FP, ?TP, ?AP, ?MP.
+    unfold id, eq_rect in *.
+    repeat eq_refl_match.
+    repeat rewrite prim_to_model_to_prim, model_to_prim_to_model.
+    unfold FM.mul, FM.add, FM.from_Z, FM.to_Z.
+    simpl.
+    (* Search _ ((_ * (_ + _))%Z). *)
+    f_equal.
+    Search ((_ mod _)%Z).
+    Search (Z.add (Z.rem _ _) (Z.rem _ _)).
+    rewrite Z.mul_add_distr_l.
+  Abort.
+
+End Int63_Proofs.
