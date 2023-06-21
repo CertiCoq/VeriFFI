dc0f5713ab5e98e893274e7953d027ed9889b824
Joomy Korkut <joomy@cs.princeton.edu>
2023-06-21 06:26:54 -0400

    Compose example

---
+++ examples/compose/prog.v
@@ -0,0 +1,21 @@
+Module Type Compose.
+  Axiom compose : forall {A B C}, (B -> C) -> (A -> B) -> (A -> C).
+End Compose.
+
+
+Module C : Compose.
+  Axiom compose : forall {A B C}, (B -> C) -> (A -> B) -> (A -> C).
+End C.
+
+(* Definition prog := C.compose (fun x => x + 1) (fun y => y + 1) 2. *)
+Definition prog := C.compose S S 2.
+
+From CertiCoq.Plugin Require Import CertiCoq.
+CertiCoq Compile -cps prog
+  Extract Constants [
+    C.compose => "compose" with tinfo
+  ]
+  Include [ "prims.h" ].
+CertiCoq Generate Glue -cps -file "glue" [ nat ].
+(*
+*)
