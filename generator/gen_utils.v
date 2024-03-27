653e5482d404307cf955518c7a1041da4abc6cab
Andrew Appel <appel@princeton.edu>
2024-03-27 09:53:43 -0400

    Update BUILD script and fix the tArray FIXMEs in gen_utils

--- generator/gen_utils.v
+++ generator/gen_utils.v
@@ -330,7 +330,11 @@ Module DB.
             ret (tCoFix mfix' idx)
         | tInt p => ret (tInt p)
         | tFloat p => ret (tFloat p)
-        | tArray lev al x y => ret (tArray lev al x y) (* FIXME !!! *)
+        | tArray u arr default type =>
+             arr' <- monad_map (go ctx) arr ;;
+             default' <- go ctx default ;;
+             type' <- go ctx type ;;
+             ret (tArray u arr' default' type')
         end
     in go ctx t.
 
@@ -410,7 +414,11 @@ Module DB.
             ret (tCoFix mfix' idx)
         | tInt p => ret (tInt p)
         | tFloat p => ret (tFloat p)
-        | tArray lev al x y => ret (tArray lev al x y) (* FIXME !!! *)
+        | tArray u arr default type =>
+             arr' <- monad_map (go ctx) arr ;;
+             default' <- go ctx default ;;
+             type' <- go ctx type ;;
+             ret (tArray u arr' default' type')
         end
     in go ctx t.
 
